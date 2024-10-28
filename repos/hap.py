15534511436a2c5867651f526aea655b0649f80b
Yucheng Zhang <Yucheng.Zhang@tufts.edu>
2024-10-28 16:13:19 -0400

    create workflow

---
+++ repos/hap.py
@@ -0,0 +1,5 @@
+Description: Hap.py is a tool to compare diploid genotypes at haplotype level.
+Home Page: https://github.com/Illumina/hap.py
+Programs: bamstats.py,cnx.py,ftx.py,guess-ploidy.py,hap.py,ovc.py,plot-roh.py,pre.py,qfy.py,som.py,varfilter.py
+
+version("0.3.9", uri="docker://pkrusche/hap.py:v0.3.9")
---
+++ repos/velocyto.py
@@ -0,0 +1,5 @@
+Description: Velocyto.py is a tool used for RNA velocity estimation in Python.
+Home Page: https://github.com/velocyto-team/velocyto.py
+Programs: velocyto,python,python3
+
+version("0.17.17", uri="docker://quay.io/biocontainers/velocyto.py:0.17.17--py39h5371cbf_5")
