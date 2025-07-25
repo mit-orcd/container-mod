4db75b0e5b0ccc6783e545ed82d6bc70566ec854
Yucheng Zhang <Yucheng.Zhang@tufts.edu>
2025-07-25 12:45:36 -0400

    script can change permission to 755 of outputs

--- templates/module_template.lua
+++ templates/module_template.lua
@@ -1,8 +1,6 @@
 -- The MIT License (MIT)
 --
--- Copyright (c) 2024 Tufts University
--- Copyright (c) 2021 Purdue University
--- Copyright (c) 2020 NVIDIA Corporation
+-- Copyright (c) 2025 Tufts University
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to
